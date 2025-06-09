1f6f1b77b2b7de99c781777fa0e4897449209033
rohanpatil1794 <rohanpatil1794@gmail.com>
2025-06-09 19:30:40 +0530

    UI and Readme file updated

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
-# Medical-Chatbot-Gen-AI
+# MHow to Run?
 
-## Step 01: Creat a conda environment after opening the repo
+### Step 01: Create a conda environment after opening the repo
 
-'''bash
+```bash
 conda create -n medibot python=3.10 -y
-'''
-'''bash
+```
+```bash
 conda activate medibot
-'''
+```
 
-## Step 02: Install the requirmenets
-'''bash
+### Step 02: Install the requirements
+
+```bash
 pip install -r requirements.txt
-'''
+```
+
+### Create a `.env` file in the root directory and add your Pinecone and openai credentials as follows:
+
+```ini
+PINECONE_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+OPENAI_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+```
+
+```bash
+# run the following command to store embeddings to pinecone
+python store_index.py
+```
+
+```bash
+# Finally run the following command
+python app.py
+```
+
+Now, 
+```bash
+open up localhost:
+```
+
+### Techstack Used:
+
+- Python
+- LangChain
+- Flask
+- GPT
+- Pinecone
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from src.prompt import *
+from src.prompt import system_prompt
 import os
 
 app = Flask(__name__)
@@ -20,6 +20,9 @@ os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 embeddings = download_hugging_face_embeddings()
+
+index_name = "medicalbot"
+
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
@@ -42,11 +45,10 @@ rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 def index():
     return render_template('chat.html')
 
-app.route('/get', methods=["GET", "POST"])
+@app.route('/get', methods=["GET", "POST"])
 def chat():
     msg = request.form['msg']
-    input = msg
-    print(input)
+    print(msg)
     response = rag_chain.invoke({"input": msg})
     print("Response:", response["answer"])
     return str(response["answer"])
